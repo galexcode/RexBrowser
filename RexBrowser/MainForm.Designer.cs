e4e4dfab6f91eb843a5323aa750c45165ea48d71
Rex Grammer <matija.cupic.97@gmail.com>
2012-06-28 19:26:10 +0100

    Added tabing (sort of)

--- RexBrowser/MainForm.Designer.cs
+++ RexBrowser/MainForm.Designer.cs
@@ -28,19 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.pnlControls = new System.Windows.Forms.Panel();
+            this.tbAddress = new System.Windows.Forms.TextBox();
             this.msMain = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.helloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.button1 = new System.Windows.Forms.Button();
             this.tpMain = new System.Windows.Forms.TabPage();
             this.wkbMain = new WebKit.WebKitBrowser();
             this.tcMain = new System.Windows.Forms.TabControl();
-            this.btnRefresh = new System.Windows.Forms.Button();
+            this.pnlControls.SuspendLayout();
             this.msMain.SuspendLayout();
             this.tpMain.SuspendLayout();
             this.tcMain.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Home";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(69, 6);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(62, 42);
+            this.btnRefresh.TabIndex = 4;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // pnlControls
+            // 
+            this.pnlControls.Controls.Add(this.tbAddress);
+            this.pnlControls.Controls.Add(this.btnRefresh);
+            this.pnlControls.Controls.Add(this.button1);
+            this.pnlControls.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlControls.Location = new System.Drawing.Point(0, 24);
+            this.pnlControls.Name = "pnlControls";
+            this.pnlControls.Size = new System.Drawing.Size(562, 58);
+            this.pnlControls.TabIndex = 5;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(155, 18);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(386, 20);
+            this.tbAddress.TabIndex = 5;
             // 
             // msMain
             // 
@@ -49,39 +89,22 @@
             this.msMain.Location = new System.Drawing.Point(0, 0);
             this.msMain.Name = "msMain";
             this.msMain.Size = new System.Drawing.Size(562, 24);
-            this.msMain.TabIndex = 1;
-            this.msMain.Text = "msMain";
+            this.msMain.TabIndex = 6;
+            this.msMain.Text = "menuStrip1";
             // 
             // fileToolStripMenuItem
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.helloToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
-            // helloToolStripMenuItem
-            // 
-            this.helloToolStripMenuItem.Name = "helloToolStripMenuItem";
-            this.helloToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
-            this.helloToolStripMenuItem.Text = "Hello";
-            // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
             this.exitToolStripMenuItem.Text = "Exit";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(53, 27);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Home";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.btnTest_Click);
             // 
             // tpMain
             // 
@@ -89,53 +112,48 @@
             this.tpMain.Location = new System.Drawing.Point(4, 22);
             this.tpMain.Name = "tpMain";
             this.tpMain.Padding = new System.Windows.Forms.Padding(3);
-            this.tpMain.Size = new System.Drawing.Size(554, 327);
+            this.tpMain.Size = new System.Drawing.Size(554, 348);
             this.tpMain.TabIndex = 0;
+            this.tpMain.Text = "tabPage1";
             this.tpMain.UseVisualStyleBackColor = true;
             // 
             // wkbMain
             // 
             this.wkbMain.BackColor = System.Drawing.Color.White;
+            this.wkbMain.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.wkbMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.wkbMain.Location = new System.Drawing.Point(3, 3);
             this.wkbMain.Name = "wkbMain";
-            this.wkbMain.Size = new System.Drawing.Size(548, 321);
+            this.wkbMain.Size = new System.Drawing.Size(548, 342);
             this.wkbMain.TabIndex = 0;
             this.wkbMain.Url = null;
             this.wkbMain.DocumentTitleChanged += new System.EventHandler(this.wkbMain_DocumentTitleChanged);
+            this.wkbMain.NewWindowRequest += new WebKit.NewWindowRequestEventHandler(this.wkbMain_NewWindowRequest);
             // 
             // tcMain
             // 
             this.tcMain.Controls.Add(this.tpMain);
             this.tcMain.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.tcMain.Location = new System.Drawing.Point(0, 109);
+            this.tcMain.Location = new System.Drawing.Point(0, 88);
             this.tcMain.Name = "tcMain";
             this.tcMain.SelectedIndex = 0;
-            this.tcMain.Size = new System.Drawing.Size(562, 353);
-            this.tcMain.TabIndex = 2;
-            // 
-            // btnRefresh
-            // 
-            this.btnRefresh.Location = new System.Drawing.Point(151, 27);
-            this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
-            this.btnRefresh.TabIndex = 4;
-            this.btnRefresh.Text = "Refresh";
-            this.btnRefresh.UseVisualStyleBackColor = true;
-            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            this.tcMain.Size = new System.Drawing.Size(562, 374);
+            this.tcMain.TabIndex = 7;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(562, 462);
-            this.Controls.Add(this.btnRefresh);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.tcMain);
+            this.Controls.Add(this.pnlControls);
             this.Controls.Add(this.msMain);
             this.MainMenuStrip = this.msMain;
             this.Name = "MainForm";
             this.Text = "RexBrowser";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.pnlControls.ResumeLayout(false);
+            this.pnlControls.PerformLayout();
             this.msMain.ResumeLayout(false);
             this.msMain.PerformLayout();
             this.tpMain.ResumeLayout(false);
@@ -147,15 +165,16 @@
 
         #endregion
 
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Panel pnlControls;
         private System.Windows.Forms.MenuStrip msMain;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem helloToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbAddress;
         private System.Windows.Forms.TabPage tpMain;
         private WebKit.WebKitBrowser wkbMain;
         private System.Windows.Forms.TabControl tcMain;
-        private System.Windows.Forms.Button btnRefresh;
     }
 }
 
--- RexBrowser/MainForm.cs
+++ RexBrowser/MainForm.cs
@@ -14,6 +14,7 @@ namespace RexBrowser
         public MainForm()
         {
             InitializeComponent();
+            ResizeBrowser();
         }
 
         private void btnTest_Click(object sender, EventArgs e)
@@ -24,11 +25,32 @@ namespace RexBrowser
         private void wkbMain_DocumentTitleChanged(object sender, EventArgs e)
         {
             tpMain.Text = wkbMain.DocumentTitle;
+            tbAddress.Text = wkbMain.Url.ToString();
         }
 
         private void btnRefresh_Click(object sender, EventArgs e)
         {
             wkbMain.Refresh();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            ResizeBrowser();
+        }
+
+        private void ResizeBrowser()
+        {
+            tcMain.Height = this.Height - msMain.Height - pnlControls.Height - 50;
+        }
+
+        private void wkbMain_NewWindowRequest(object sender, WebKit.NewWindowRequestEventArgs e)
+        {
+            WebBrowser wkbSecondary = new WebBrowser();
+            wkbSecondary.Dock= DockStyle.Fill;
+            wkbSecondary.Url = new Uri(e.Url);
+            TabPage newPage = new TabPage(wkbSecondary.DocumentTitle);
+            newPage.Controls.Add(wkbSecondary);
+            tcMain.TabPages.Add(newPage);
         }
     }
 }
